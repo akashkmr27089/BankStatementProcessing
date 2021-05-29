576c2122b6dae9b5543f6880562789341fc22925
Aakash Kumar <akashkmr27089@gmail.com>
2021-05-29 10:50:25 +0530

    Add files via upload

---
+++ CONFIG.py
@@ -0,0 +1,13 @@
+Bank = {
+    "SBI": {
+        "title": 19,
+        "Data_start": 20,
+        "Data_end": -3
+    }
+}
+
+BankName = {"SBI"}
+
+Appsetting = {
+    "FileGenericLocation": "./Files/",
+}
